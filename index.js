99375a0040ffc01fc9cc1f28845cd93b6900e08f
Keith Wade <keawade@gmail.com>
2016-06-27 23:12:59 -0500

    Auth working
    TODO: implement styling and user identification

--- index.js
+++ index.js
@@ -3,23 +3,26 @@ var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var socketioJwt = require('socketio-jwt')
-require('dotenv').config()
+var dotenv = require('dotenv')
 
+dotenv.load()
+
+var env = {
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN
+};
+var port = process.env.PORT || 3000
+
+app.set('views', __dirname + '/views')
 app.set('view engine', 'pug')
-app.set('port', process.env.PORT || 3000)
-app.set('ip', process.env.IP || 'localhost')
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
 app.locals.sitename = 'chattr-auth0'
 app.locals.source_url = 'https://github.com/keawade/chattr-auth0'
 
-app.get('/', function (req, res) {
-    res.render('chat')
-})
-
 io
     .on('connection', socketioJwt.authorize({
-        secret: Buffer(process.env.AUTH0_SECRET, 'base64'),
+        secret: Buffer(process.env.AUTH0_CLIENT_SECRET, 'base64'),
         timeout: 15000 // 15 seconds to send the authentication message
     })).on('authenticated', function (socket) {
         console.log('[auth] socket authenticated', JSON.stringify(socket.decoded_token))
@@ -28,6 +31,10 @@ io
             });
     })
 
-http.listen(3000, function () {
-    console.log('[server] listening at port 3000')
+app.get('/', function (req, res) {
+  res.render('chat', { env: env })
+})
+
+http.listen(port, function(){
+  console.log('listening on *:' + port)
 })
